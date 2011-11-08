ed64147d70414b2810e642d3c8fcf127f0ec1ec4
zwc1986@gmail.com <zwc1986@gmail.com@1d2a83ab-9210-0493-9b96-02f2f31171d8>
2011-11-08 04:44:00 +0000

    发布第一个版本

---
+++ HandlerSample/HandlerSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+
+// DLL名称: name.zwc.Caller.Handlers.HandlerSample
+// 名空间: name.zwc.Caller.Handlers
+// 类名: HandlerSample
+// 方法定义: public static string SayHello(string input, Dictionary<string, string> args)
+namespace name.zwc.Caller.Handlers
+{
+    public class HandlerSample
+    {
+        public static string SayHello(string input, Dictionary<string, string> args)
+        {
+            String result = "Hello " + args["name"];
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendFormat("input:<br/>{0}<br/><br/>", input);
+            builder.AppendFormat("return:<br/>{0}<br/><br/>", result);
+            builder.AppendLine("args:<br/>");
+            foreach (string key in args.Keys)
+            {
+                builder.AppendLine(key + ": " + args[key] + "<br/>");
+            }
+            return builder.ToString();
+        }
+    }
+}
