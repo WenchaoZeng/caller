b9fc87fe3ecded4317ddbbb48c728d3a8c6fb5d3
zwc1986@gmail.com <zwc1986@gmail.com@1d2a83ab-9210-0493-9b96-02f2f31171d8>
2011-11-08 04:44:00 +0000

    发布第一个版本

---
+++ trunk/Caller/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Caller")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Caller")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc8f2198-b34e-45db-9972-161b8373c46e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ trunk/Caller/call.ashx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Reflection;
+using System.IO;
+using System.Text;
+
+namespace name.zwc.Caller
+{
+    public class call : IHttpHandler
+    {
+        public readonly String HandlerNamesapce = "name.zwc.Caller.Handlers";
+
+        public void ProcessRequest(HttpContext context)
+        {
+            String handlerName = context.Request.QueryString["h"];
+            String methodName = context.Request.QueryString["m"];
+
+            // 参数
+            String input = new StreamReader(context.Request.InputStream, Encoding.UTF8).ReadToEnd();
+            Dictionary<String, String> args = new Dictionary<String, String>();
+            foreach (String key in context.Request.Params.Keys)
+            {
+                args.Add(key, context.Request.Params[key]);
+            }
+
+            // 构建处理器
+            Assembly assembly = Assembly.Load(HandlerNamesapce + "." + handlerName);
+            Type type = assembly.GetType(HandlerNamesapce + "." + handlerName);
+            MethodInfo method = type.GetMethod(methodName);
+
+            String result = (String)method.Invoke(null, new object[] { input, args });
+
+            context.Response.Write(result);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
